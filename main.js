c069f9d2119d18fd2271aa579251221afd365c5c
Yuzunoha <studysteadystandby@gmail.com>
2019-12-25 18:07:20 +0900

    雛形

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const urlSignUp = 'https://teachapi.herokuapp.com/sign_up';
+const urlSignIn = 'https://teachapi.herokuapp.com/sign_in';
+const urlUsers = 'https://teachapi.herokuapp.com/users';
+const urlPosts = 'https://teachapi.herokuapp.com/posts';
+
+console.log(urlUsers);
