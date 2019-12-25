8be33e9b12fd3bc06bc2ee0fc4e7ceac78303783
Yuzunoha <nurugamer2@gmail.com>
2019-12-26 02:45:14 +0900

    保存しただけ。自動整形

--- main.js
+++ main.js
@@ -6,57 +6,65 @@ const urlUsers = 'https://teachapi.herokuapp.com/users';
 const urlPosts = 'https://teachapi.herokuapp.com/posts';
 
 const onclickButtonSignUp = () => {
-  const name = document.getElementById("signUpName").value;
-  const bio = document.getElementById("signUpBio").value;
-  const email = document.getElementById("signUpEmail").value;
-  const password = document.getElementById("signUpPassword").value;
-  const passwordConfirmation = document.getElementById("signUpPasswordConfirmation").value;
+  const name = document.getElementById('signUpName').value;
+  const bio = document.getElementById('signUpBio').value;
+  const email = document.getElementById('signUpEmail').value;
+  const password = document.getElementById('signUpPassword').value;
+  const passwordConfirmation = document.getElementById('signUpPasswordConfirmation').value;
   const bodyObj = {
-    "sign_up_user_params": {
-      "name": name,
-      "bio": bio,
-      "email": email,
-      "password": password,
-      "password_confirmation": passwordConfirmation
+    sign_up_user_params: {
+      name: name,
+      bio: bio,
+      email: email,
+      password: password,
+      password_confirmation: passwordConfirmation
     }
-  }
-  const method = "POST";
+  };
+  const method = 'POST';
   const body = JSON.stringify(bodyObj);
   const headers = {
     'Content-Type': 'application/json'
   };
-  fetch(urlSignUp, { method, headers, body })
-    .then((res) => res.json())
+  fetch(urlSignUp, {
+    method,
+    headers,
+    body
+  })
+    .then(res => res.json())
     .then(resJson => {
-      console.log(resJson)
+      console.log(resJson);
       // 返却されたトークンを保存する
       localStorage.token = resJson.token;
     })
     .catch(console.error);
-}
+};
 
 const onclickButtonSignIn = () => {
-  const email = document.getElementById("signInEmail").value;
-  const password = document.getElementById("signInPassword").value;
-  const passwordConfirmation = document.getElementById("signInPasswordConfirmation").value;
+  const email = document.getElementById('signInEmail').value;
+  const password = document.getElementById('signInPassword').value;
+  const passwordConfirmation = document.getElementById('signInPasswordConfirmation').value;
   const bodyObj = {
-    "sign_in_user_params": {
-      "email": email,
-      "password": password,
-      "password_confirmation": passwordConfirmation
+    sign_in_user_params: {
+      email: email,
+      password: password,
+      password_confirmation: passwordConfirmation
     }
-  }
-  const method = "POST";
+  };
+  const method = 'POST';
   const body = JSON.stringify(bodyObj);
   const headers = {
     'Content-Type': 'application/json'
   };
-  fetch(urlSignIn, { method, headers, body })
-    .then((res) => res.json())
+  fetch(urlSignIn, {
+    method,
+    headers,
+    body
+  })
+    .then(res => res.json())
     .then(resJson => {
-      console.log(resJson)
+      console.log(resJson);
       // 返却されたトークンを保存する
       localStorage.token = resJson.token;
     })
     .catch(console.error);
-}
+};
