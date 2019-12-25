0f5eeacda21210be7e4529bd419b0b2af239c6cf
Yuzunoha <studysteadystandby@gmail.com>
2019-12-25 21:50:05 +0900

    ボタン押下で値を取得してJS実行

--- main.js
+++ main.js
@@ -5,4 +5,21 @@ const urlSignIn = 'https://teachapi.herokuapp.com/sign_in';
 const urlUsers = 'https://teachapi.herokuapp.com/users';
 const urlPosts = 'https://teachapi.herokuapp.com/posts';
 
-console.log(urlUsers);
+const onclickButtonSignIn = () => {
+  const name = document.getElementById("singInName").value;
+  const bio = document.getElementById("singInBio").value;
+  const email = document.getElementById("singInEmail").value;
+  const password = document.getElementById("singInPassword").value;
+  const passwordConfirmation = document.getElementById("singInPasswordConfirmation").value;
+  const body = {
+    "sign_up_user_params": {
+      "name": name,
+      "bio": bio,
+      "email": email,
+      "password": password,
+      "password_confirmation": passwordConfirmation
+    }
+  }
+  console.log(body);
+}
+
